e27d1a71e349323e9bb48e0e8b474851eb2f56f8
axwell-wq <Stepan07mail@gmail.com>
2025-03-26 09:32:04 +0300

    написал метод create

--- src/main/java/faang/school/projectservice/service/moment/MomentServiceImpl.java
+++ src/main/java/faang/school/projectservice/service/moment/MomentServiceImpl.java
@@ -1,6 +1,14 @@
 package faang.school.projectservice.service.moment;
 
+import faang.school.projectservice.dto.client.MomentDto;
+import faang.school.projectservice.mapper.MomentDtoMapper;
+import faang.school.projectservice.model.Moment;
+import faang.school.projectservice.model.Project;
+import faang.school.projectservice.model.ProjectVisibility;
+import faang.school.projectservice.repository.MomentRepository;
+import faang.school.projectservice.repository.ProjectJpaRepository;
 import faang.school.projectservice.service.MomentService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +16,20 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MomentServiceImpl implements MomentService {
 
+    private final MomentRepository momentRepository;
+    private final ProjectJpaRepository projectJpaRepository;
+    private final MomentDtoMapper mapper;
 
+    public MomentDto createMoment(MomentDto momentDto, Long projectId) {
+        Project project = projectJpaRepository.findById(projectId).orElseThrow(
+                () -> new EntityNotFoundException("Проект не найден"));
+
+        if (!project.getVisibility().equals(ProjectVisibility.PUBLIC)) {
+            throw new EntityNotFoundException("Проект закрыт для вас");
+        }
+
+        Moment moment = momentRepository.save(mapper.toEntity(momentDto));
+
+        return mapper.toDto(moment);
+    }
 }
