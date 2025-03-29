3918d508b7a81cb77835202ca5970d20aa0a5c23
axwell-wq <Stepan07mail@gmail.com>
2025-03-29 18:00:36 +0300

    не работает маппер, нужно исправить и доделать пару методов

--- src/main/java/faang/school/projectservice/service/stage/StageServiceImpl.java
+++ src/main/java/faang/school/projectservice/service/stage/StageServiceImpl.java
@@ -1,11 +1,14 @@
 package faang.school.projectservice.service.stage;
 
+import faang.school.projectservice.dto.client.ProjectDto;
 import faang.school.projectservice.dto.client.StageDto;
 import faang.school.projectservice.dto.client.StageFilterDto;
 import faang.school.projectservice.dto.client.StageRolesDto;
+import faang.school.projectservice.mapper.ProjectDtoMapper;
 import faang.school.projectservice.mapper.StageDtoMapper;
 import faang.school.projectservice.mapper.StageRolesMapper;
 import faang.school.projectservice.model.Project;
+import faang.school.projectservice.model.Task;
 import faang.school.projectservice.model.stage.Stage;
 import faang.school.projectservice.model.stage.StageRoles;
 import faang.school.projectservice.repository.ProjectJpaRepository;
@@ -26,6 +29,7 @@ public class StageServiceImpl implements StageService {
     private final ProjectJpaRepository projectJpaRepository;
     private final StageDtoMapper stageMapper;
     private final StageRolesMapper stageRolesMapper;
+    private final ProjectDtoMapper projectDtoMapper;
 
     public StageDto createStage(StageDto stageDto) {
         Project project = projectJpaRepository.findById(stageDto.getProjectId()).orElseThrow(
@@ -48,5 +52,33 @@ public class StageServiceImpl implements StageService {
         return stageList.stream()
                 .map(stageMapper::toDto)
                 .toList();
+    }
+
+    public void deleteStage(Long stageId) {
+        if (stageJpaRepository.existsById(stageId)) {
+            stageJpaRepository.deleteById(stageId);
+        } else {
+            throw new EntityNotFoundException("Нет такого проекта");
+        }
+    }
+
+    public void TaskTransfer(Long stageId, Long stageTransferId) {
+        Stage stage = stageJpaRepository.findById(stageId).orElseThrow(
+                () -> new EntityNotFoundException("Нет такого проекта"));
+
+        Stage stageTransfer = stageJpaRepository.findById(stageTransferId).orElseThrow(()
+                -> new EntityNotFoundException("Нет такого проекта"));
+
+        List<Task> taskList = stage.getTasks();
+        List<Task> taskListTransfer = stageTransfer.getTasks();
+        taskListTransfer.addAll(taskList);
+    }
+
+    public List<StageDto> getAllStage(ProjectDto projectDto) {
+        Project project = projectJpaRepository.findById(projectDto.getId()).orElseThrow(
+                () -> new EntityNotFoundException("Проект не найден"));
+
+        List<Stage> stageList = project.getStages();
+        return projectDtoMapper.toDto(stageList);
     }
 }
